21167ee0fbfaa1a93874caa094b7a65ff8ab3615
jasbin <jasbinkarki@gmail.com>
2019-06-02 16:04:40 +0545

    UI fixes

--- public/js/backend.js
+++ public/js/backend.js
@@ -1804,9 +1804,9 @@ $(function () {
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! A:\Booking\resources\js\backend\before.js */"./resources/js/backend/before.js");
-__webpack_require__(/*! A:\Booking\resources\js\backend\app.js */"./resources/js/backend/app.js");
-module.exports = __webpack_require__(/*! A:\Booking\resources\js\backend\after.js */"./resources/js/backend/after.js");
+__webpack_require__(/*! A:\Boilerplatematerial\resources\js\backend\before.js */"./resources/js/backend/before.js");
+__webpack_require__(/*! A:\Boilerplatematerial\resources\js\backend\app.js */"./resources/js/backend/app.js");
+module.exports = __webpack_require__(/*! A:\Boilerplatematerial\resources\js\backend\after.js */"./resources/js/backend/after.js");
 
 
 /***/ })
